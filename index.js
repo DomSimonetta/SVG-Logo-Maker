150be1f7a282d7bb84cc210aadeee0d12a753928
DomSimonetta <Guitardudedom@gmail.com>
2024-03-28 15:46:21 -0400

    added basic files and packages needed for project

---
+++ index.js
@@ -0,0 +1,11 @@
+const inquirer = require("inquirer");
+const jest = require("jest");
+
+const questions = [
+    {
+        type: "input",
+        name: "Characters",
+        message: "Input up to 3 characters"
+    },
+    
+]
